97d5396d87320c490176f38246b2f9862648e877
Khushi <khushisardhara7@gmail.com>
2023-02-11 15:21:44 +0530

    Changes carousel

--- Agrishop/wsgi.py
+++ Agrishop/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for VegetableShop project.
+WSGI config for AgriShop project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VegetableShop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Agrishop.settings')
 
 application = get_wsgi_application()
